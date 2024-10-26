a059da2339f7557eb8be5913db50f458bbf72102
miwa <k023c0026@g.neec.ac.jp>
2024-10-26 16:15:31 +0900

    ok

---
+++ Numpy/aggregate.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([2,4,9])
+b = np.array([2,1,0])
+
+print(np.max(a))
+print(np.sum(a))
+print(np.mean(a))
+print(np.std(a))
---
+++ Numpy/universal.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([2,4,9])
+b = np.array([2,1,0])
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(np.dot(a,b))#内積
+print(a==a)
+print(a==b)
+print(np.sin(a))
+print(np.log(a))
+print(np.exp(a))
